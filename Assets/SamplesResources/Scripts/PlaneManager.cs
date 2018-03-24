57f8b7beadc7f6b51cb1e9e7719cffc53369ff0b
eliosco <eliasdjpizza@hotmail.it>
2018-03-24 11:40:06 +0100

    AGGIORNAMENTO 24/03
    verso la fine

--- Assets/SamplesResources/Scripts/PlaneManager.cs
+++ Assets/SamplesResources/Scripts/PlaneManager.cs
@@ -23,14 +23,14 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
     // public MidAirPositionerBehaviour m_MidAirPositioner;
     public TrackableBehaviour imageTrackable;
     public GameObject m_PlaneAugmentation;// m_MidAirAugmentation;
-    public UnityEngine.UI.Text m_TitleMode;
+   // public UnityEngine.UI.Text m_TitleMode;
     public UnityEngine.UI.Text m_OnScreenMessage;
     public UnityEngine.UI.Image m_PlaneModeIcon;
    // public UnityEngine.UI.Toggle m_GroundToggle;// m_MidAirToggle;
-    //public UnityEngine.UI.Button m_ResetButton;
+    public UnityEngine.UI.Button m_ResetButton;
     public CanvasGroup m_GroundReticle;
     public GameObject cube;
-    public GameObject Buttonreset;
+    //public GameObject Buttonreset;
     #endregion // PUBLIC_MEMBERS
 
 
@@ -62,12 +62,12 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
         DeviceTrackerARController.Instance.RegisterDevicePoseStatusChangedCallback(OnDevicePoseStatusChanged);
         m_PlaneFinder.enabled = false;
 
-        m_TitleMode.text = TITLE_GROUNDPLANE;
+       // m_TitleMode.text = TITLE_GROUNDPLANE;
         m_PlaneFinder.HitTestMode = HitTestMode.INTERACTIVE;
 
       //  m_GroundToggle.interactable = false;
        // m_MidAirToggle.interactable = false;
-      //  m_ResetButton.interactable = false;
+       m_ResetButton.interactable = false;
 
         m_IconGroundMode = Resources.Load<Sprite>("icon_ground_mode");
         // m_IconMidAirMode = Resources.Load<Sprite>("icon_midair_mode");
@@ -79,17 +79,21 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
 
     void LateUpdate()
     {
-        if (0 == Time.frameCount)
+        /*if (0 == Time.frameCount)
         {
             // We got an automatic hit test this frame
             m_GroundReticle.alpha = 0; // hide the onscreen reticle
             m_OnScreenMessage.enabled = false; // hide the onscreen message
             SetSurfaceIndicatorVisible(true); // display the surface indicator
         }
-        else
-        {
-            // No automatic hit test, so set alpha based on which plane mode is active
-            m_GroundReticle.alpha = (planeMode == PlaneMode.GROUND) ? 1 : 0;
+        else*/
+        //   
+        if (0 == Time.frameCount) { 
+            m_GroundReticle.alpha = 0; // hide the onscreen reticle
+        SetSurfaceIndicatorVisible(true);}// display the surface indicator
+        else{
+                                          // No automatic hit test, so set alpha based on which plane mode is active
+        m_GroundReticle.alpha = (planeMode == PlaneMode.GROUND) ? 1 : 0;
 
             if (planeMode == PlaneMode.GROUND)
             {
@@ -101,8 +105,8 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
                 m_OnScreenMessage.enabled = true;
                 m_OnScreenMessage.text = "Touch anywhere on screen";
             }*/
-            SetSurfaceIndicatorVisible(false);
-        }
+           // SetSurfaceIndicatorVisible(false);
+       }
     }
 
     void OnDestroy()
@@ -124,15 +128,15 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
         Debug.Log("OnVuforiaStarted() called.");
 
         stateManager = TrackerManager.Instance.GetStateManager();
-       imageTrackable.RegisterTrackableEventHandler(this);
+      imageTrackable.RegisterTrackableEventHandler(this);
     }
 
     void OnVuforiaPaused(bool paused)
     {
         Debug.Log("OnVuforiaPaused(" + paused.ToString() + ") called.");
 
-        if (paused)
-            Reset();
+        //if (paused)
+         //   Reset();
     }
 
     #endregion // VUFORIA_CALLBACKS
@@ -196,14 +200,16 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
 
     private void RotateTowardImageTarget(GameObject augmentation)
     {
-        //var lookAtPosition = cube.transform.position - augmentation.transform.position;
+        var lookAtPosition = cube.transform.position;// - augmentation.transform.position;
         //lookAtPosition.y = 0;
 
         //var lookAtPositionTarget = imageTarget.transform.position;
         //var rotation = Quaternion.LookRotation(lookAtPosition);
+        Debug.Log("rotateToward");
         var rotation = cube.transform.rotation;
         rotation.x = 0;
         rotation.z = 0;
+        //rotation.y =180;
 
         augmentation.transform.rotation = rotation;
     }
@@ -229,6 +235,7 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
     public void HandleInteractiveHitTest(HitTestResult result)
     {
         Debug.Log("HandleInteractiveHitTest() called.");
+        
 
         if (result == null)
         {
@@ -238,9 +245,9 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
 
         // Place object based on Ground Plane mode
         switch (planeMode)
-        {
+        {  
             case PlaneMode.GROUND:
-
+                Debug.Log("case PlaneGround");
                 if (positionalDeviceTracker != null && positionalDeviceTracker.IsActive)
                 {
                     
@@ -248,7 +255,7 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
 
                     contentPositioningBehaviour = m_PlaneFinder.GetComponent<ContentPositioningBehaviour>();
                     contentPositioningBehaviour.PositionContentAtPlaneAnchor(result);
-                    Debug.Log("mannaggiasanda");
+                    // Debug.Log("mannaggiasanda");
 
                     /*if (!m_MidAirToggle.interactable)
                     {
@@ -256,26 +263,29 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
                         m_MidAirToggle.interactable = true;
                         m_ResetButton.interactable = true;
                     }*/
-                 //  m_ResetButton.interactable = true;
+                    m_ResetButton.interactable = true;
+                    //Buttonreset.SetActive(true);
+
                 }
 
                 if (!m_PlaneAugmentation.activeInHierarchy)
                 {
-                    Debug.Log("Setting Plane Augmentation to Active");
+                    //Debug.Log("Setting Plane Augmentation to Active");
                     // On initial run, unhide the augmentation
-                    m_PlaneAugmentation.transform.localPosition = Vector3.zero;
-                   // m_PlaneAugmentation.transform.rotation = Quaternion.identity;
+                    //m_PlaneAugmentation.transform.localPosition = Vector3.zero;
+                    //m_PlaneAugmentation.transform.rotation = Quaternion.identity;
                     m_PlaneAugmentation.SetActive(true);
                 }
+                
 
-                Debug.Log("Positioning Plane Augmentation at: " + result.Position);
+                Debug.Log("Positioning Plane Augmentation at: " + cube.transform.position);
                 m_PlaneAugmentation.PositionAt(cube.transform.position);
                 RotateTowardImageTarget(m_PlaneAugmentation);
 
                 break;
 
             default:
-                Debug.LogError("Invalid Ground Plane state: " + planeMode);
+               // Debug.LogError("Invalid Ground Plane state: " + planeMode);
                 break;
         }
     }
@@ -317,9 +327,9 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
         if (active)
         {
             planeMode = PlaneMode.GROUND;
-            m_TitleMode.text = TITLE_GROUNDPLANE;
+           // m_TitleMode.text = TITLE_GROUNDPLANE;
             m_PlaneModeIcon.sprite = m_IconGroundMode;
-           // m_PlaneFinder.gameObject.SetActive(true);
+            m_PlaneFinder.gameObject.SetActive(true);
           //  m_MidAirPositioner.gameObject.SetActive(false);
         }
     }
@@ -338,13 +348,13 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
 
     public void Reset()
     {
-        Debug.Log("Reset() called.");
+        //Debug.Log("Reset() called.");
 
         // reset augmentations
         m_PlaneAugmentation.transform.position = Vector3.zero;
         m_PlaneAugmentation.transform.localEulerAngles = Vector3.zero;
         m_PlaneAugmentation.SetActive(false);
-        Buttonreset.SetActive(false);
+        //Buttonreset.SetActive(false);
         // m_MidAirAugmentation.transform.position = Vector3.zero;
         // m_MidAirAugmentation.transform.localEulerAngles = Vector3.zero;
         //  m_MidAirAugmentation.SetActive(false);
@@ -352,7 +362,7 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
         // reset buttons
         // m_GroundToggle.isOn = true;
         //m_MidAirToggle.interactable = false;
-        //m_ResetButton.interactable = false;
+        m_ResetButton.interactable = false;
         primo = false;
     }
 
@@ -371,22 +381,24 @@ public class PlaneManager : MonoBehaviour, ITrackableEventHandler
 
             if (!primo)
             {
-                Buttonreset.SetActive(true);
+                if (!m_ResetButton) m_ResetButton.interactable = true;
                 m_PlaneFinder.enabled = true;
-                //Vector2 screenPoint = mainCamera.WorldToScreenPoint(imageTrackable.transform.position);
+               // Vector2 screenPoint = mainCamera.WorldToScreenPoint(imageTrackable.transform.position);
                 Vector2 screenPoint;
-                  screenPoint=mainCamera.WorldToScreenPoint(cube.transform.position);
+                 screenPoint=mainCamera.WorldToScreenPoint(cube.transform.position);
+                Debug.Log(screenPoint);
+
                 //screenPoint.x = screenPoint.x + 500;
                 //screenPoint.y = screenPoint.y + 500;
                   m_PlaneFinder.PerformHitTest(screenPoint);
                 m_PlaneFinder.enabled = false;
 
-               // m_PlaneAugmentation.transform.position = cube.transform.position;
-              //  m_PlaneAugmentation.transform.localPosition = Vector3.zero;
-              //  m_PlaneAugmentation.transform.rotation = Quaternion.identity;
-              //  m_PlaneAugmentation.SetActive(true);
+               m_PlaneAugmentation.transform.position = cube.transform.position;
+              // m_PlaneAugmentation.transform.localPosition = Vector3.zero;
+              // m_PlaneAugmentation.transform.rotation = Quaternion.identity;
+                m_PlaneAugmentation.SetActive(true);
                 cube.SetActive(false);
-                Debug.Log("Trackable found first time");
+               Debug.Log("Trackable found first time");
                 // m_GroundToggle.isOn = true;
                 primo = true;
             }
---
+++ Assets/myScripts/ChangeScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+public void  ScenaBotteghe() {
+      
+        SceneManager.LoadScene("scenaBotteghe",LoadSceneMode.Single);
+}
+    public void ScenaMosaico()
+    {
+
+        SceneManager.LoadScene("scenaMosaico", LoadSceneMode.Single);
+    }
+
+    public void ScenaMenu() {
+        SceneManager.LoadScene("menu", LoadSceneMode.Single);
+
+
+    }
+
+    public void Esci()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+}
+
